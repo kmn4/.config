5e0b53b901d83cddaa654610b8eb525b1acbea2d
Masaki Kamano <kamano.m.aa@m.titech.ac.jp>
2021-06-30 13:16:11 +0900

    update

--- init.el
+++ init.el
@@ -2,8 +2,11 @@
   (push default-directory load-path)
   (normal-top-level-add-subdirs-to-load-path))
 
+(setq custom-file (concat user-emacs-directory "emacs-custom-settings.el"))
+(load custom-file)
+
 (setq package-archives '(("melpa" . "https://melpa.org/packages/")
-			 ("gnu" . "https://elpa.gnu.org/packages/")))
+		 ("gnu" . "https://elpa.gnu.org/packages/")))
 (package-refresh-contents t)
 (package-initialize)
 
@@ -81,10 +84,13 @@
 
 (set-language-environment "Japanese")
 (prefer-coding-system 'utf-8)
+;; Ubuntu  -- apt install cmigemo
+;; Windows -- https://www.kaoriya.net/software/cmigemo/
+;; M-x customize-variable migemo-dictionary
 (package-install 'migemo)
 (require 'migemo)
+(package-install 'avy)
 (require 'avy-migemo)
-(setq migemo-dictionary "/usr/share/cmigemo/utf-8/migemo-dict")
 (require 'avy-migemo-e.g.ivy)
 (require 'avy-migemo-e.g.counsel)
 (require 'avy-migemo-e.g.swiper)
@@ -119,38 +125,3 @@
 
 (require 'server)
 (unless server-process (server-start))
-
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(TeX-view-program-selection
-   '(((output-dvi has-no-display-manager)
-      "dvi2tty")
-     ((output-dvi style-pstricks)
-      "dvips and gv")
-     (output-dvi "xdvi")
-     (output-pdf "Okular")
-     (output-html "xdg-open")))
- '(avy-migemo-function-names
-   '((counsel-clj :around avy-migemo-disable-around)
-     (counsel-grep :around counsel-grep-migemo-around)
-     counsel-grep-function-migemo counsel-grep-occur-migemo
-     (counsel-git-occur :around counsel-git-occur-migemo-around)
-     (counsel-find-file-occur :around counsel-find-file-occur-migemo-around)
-     swiper--make-overlays-migemo
-     (swiper--re-builder :around swiper--re-builder-migemo-around)
-     (ivy--regex :around ivy--regex-migemo-around)
-     (ivy--regex-ignore-order :around ivy--regex-ignore-order-migemo-around)
-     (ivy--regex-plus :around ivy--regex-plus-migemo-around)
-     ivy--highlight-default-migemo ivy-occur-revert-buffer-migemo ivy-occur-press-migemo avy-migemo-goto-char avy-migemo-goto-char-2 avy-migemo-goto-char-in-line avy-migemo-goto-char-timer avy-migemo-goto-subword-1 avy-migemo-goto-word-1 avy-migemo-isearch avy-migemo-org-goto-heading-timer avy-migemo--overlay-at avy-migemo--overlay-at-full))
- '(custom-enabled-themes '(wombat))
- '(package-selected-packages
-   '(avy-migemo migemo git-gutter+ magit s dash magic-latex-buffer auctex undo-tree redo+ undo+ winum counsel ivy which-key lsp-ui lsp-mode)))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
