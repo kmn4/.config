483d3874e8c0c062dd7565cfedce6cc874477e22
Masaki Kamano <kamano.m.aa@m.titech.ac.jp>
2021-12-21 16:37:43 +0900

    use `leaf' to remove explicit `package-install's

--- init.el
+++ init.el
@@ -1,9 +1,5 @@
 (setq user-emacs-directory (expand-file-name (file-name-directory load-file-name)))
 
-(let ((default-directory (concat user-emacs-directory "site-lisp")))
-  (push default-directory load-path)
-  (normal-top-level-add-subdirs-to-load-path))
-
 (setq custom-file (concat user-emacs-directory "emacs-custom-settings.el"))
 (unless (file-exists-p custom-file)
   (with-temp-buffer (write-file custom-file)))
@@ -14,6 +10,10 @@
   (interactive)
   (find-file user-init-file))
 
+(let ((default-directory (concat user-emacs-directory "site-lisp")))
+  (push default-directory load-path)
+  (normal-top-level-add-subdirs-to-load-path))
+
 ;; macOS で pakcage-refresh-contents が失敗するのを直す．
 ;; 出典: https://emacs.stackexchange.com/a/68568
 (when (and (equal emacs-version "27.2")
@@ -23,17 +23,21 @@
 (setq package-archives '(("melpa" . "https://melpa.org/packages/")
                          ("gnu" . "https://elpa.gnu.org/packages/")))
 (package-refresh-contents (boundp 'package-selected-packages))
-(package-initialize)
 
-(package-install 'dash)
-(package-install 's)
-(require 'dash)
-(require 's)
+(unless (package-installed-p 'leaf) (package-install 'leaf))
+(leaf leaf-tree :ensure t)
 
-(unless (eq system-type 'windows-nt)
-  (package-install 'exec-path-from-shell)
-  (exec-path-from-shell-initialize))
-(setenv "LANG" "ja_JP.utf-8")
+(leaf util
+  :ensure dash s
+  :require dash s)
+
+(leaf envvar
+  :config
+  (leaf exec-path-from-shell :unless (eq system-type 'windows-nt)
+    :ensure t
+    :config
+    (exec-path-from-shell-initialize))
+  (setenv "LANG" "ja_JP.utf-8"))
 
 (defun shell-command-output (command)
   "COMMAND を実行し、その標準出力を文字列として返す。"
@@ -55,75 +59,80 @@
   "Dropbox sync root directory.")
 (defun +dropbox-root (path) (s-lex-format "${+dropbox-root}/${path}"))
 
-(setq confirm-kill-emacs #'yes-or-no-p)
+(leaf convenience
+  :custom
+  (confirm-kill-emacs . #'yes-or-no-p))
 
 (defmacro add-hooks (fn &rest hooks) "Add FN to each of HOOKS."
           `(let ((fn ,fn))
              (dolist (hook ',hooks)
                (add-hook hook fn))))
 
-(defun display-line-numbers-mode-on ()
-  (interactive) (display-line-numbers-mode +1))
-(add-hooks #'display-line-numbers-mode-on
-	   prog-mode-hook org-mode-hook LaTeX-mode-hook)
+(leaf display-line-numbers-mode
+  :hook prog-mode-hook org-mode-hook LaTeX-mode-hook)
 
-(add-hooks (lambda ()
-             (when (derived-mode-p 'prog-mode 'tex-mode)
-               (delete-trailing-whitespace)))
-           before-save-hook)
+(defvar delete-trailing-whitespace-modes
+  (list 'prog-mode 'tex-mode)
+  "保存前に `delete-trailing-whitespace' を呼び出すモードのリスト")
+(add-hook 'before-save-hook
+          (lambda ()
+            (when (apply #'derived-mode-p delete-trailing-whitespace-modes)
+              (delete-trailing-whitespace))))
 
-(defconst source-code-pro "Source Han Code JP-13")
-(defcustom default-font-name source-code-pro "Default font name.")
-(defun set-font (name)
-  (if-let ((spec (font-spec :name name))
-	   (font (find-font spec)))
-      (progn (set-frame-font name nil t)
-	     (setf (alist-get 'font default-frame-alist) name))
-    (message "Font not found: %s" name)))
-(set-font default-font-name)
+(leaf font
+  :config
+  (defconst source-code-pro "Source Han Code JP-13")
+  (defcustom default-font-name source-code-pro "Default font name.")
+  (defun set-font (name)
+    (if-let ((spec (font-spec :name name))
+	     (font (find-font spec)))
+        (progn (set-frame-font name nil t)
+	       (setf (alist-get 'font default-frame-alist) name))
+      (message "Font not found: %s" name)))
+  (set-font default-font-name))
 
-(with-eval-after-load 'info
+
+(leaf info
+  :config
   (setq Info-directory-list (-union Info-directory-list Info-default-directory-list))
-  (push (substitute-env-vars "$XDG_DATA_HOME/info") Info-directory-list) ; TODO
-  )
+  (push (substitute-env-vars "$XDG_DATA_HOME/info") Info-directory-list))
 
-(add-hooks (lambda () (electric-pair-local-mode +1))
-           prog-mode-hook)
-(add-hooks (lambda () (show-paren-mode +1))
-           prog-mode-hook)
-(add-hooks (lambda () (hs-minor-mode +1))
-           prog-mode-hook)
+(leaf electric-pair-local-mode :hook prog-mode-hook)
+(leaf show-paren-mode :hook prog-mode-hook)
+(leaf hs-minor-mode :hook prog-mode-hook)
 
-(setq recentf-max-saved-items 1000)
-(recentf-mode +1)
+(leaf recentf
+  :custom (recentf-max-saved-items . 1000)
+  :config (recentf-mode +1))
 
-(tool-bar-mode -1)
+(leaf tool-bar-mode :config (tool-bar-mode -1))
 
-(package-install 'undo-tree)
-(global-undo-tree-mode +1)
+(leaf undo-tree
+  :ensure t
+  :config (global-undo-tree-mode +1))
 
-(package-install 'ivy)
-(package-install 'swiper)
-(package-install 'counsel)
-(define-key global-map (kbd "C-s") 'swiper)
-(ivy-mode +1)
-(counsel-mode +1)
+(leaf ivy
+  :ensure t swiper counsel
+  :bind (("C-s" . swiper))
+  :config
+  (ivy-mode +1)
+  (counsel-mode +1))
 
-(package-install 'winum)
-(winum-mode +1)
-(let ((digit 0))
-  (while (<= digit 9)
+(leaf winum
+  :ensure t
+  :config
+  (winum-mode +1)
+  (dolist (digit (number-sequence 0 9))
     (define-key global-map (kbd (format "M-%d" digit))
-      (intern (format "winum-select-window-%d" digit)))
-    (setq digit (1+ digit))
-    ))
+      (intern (format "winum-select-window-%d" digit)))))
 
-(package-install 'which-key)
-(which-key-mode +1)
+(leaf which-key
+  :ensure t
+  :config (which-key-mode +1))
 
-(package-install 'magit)
-(package-install 'git-gutter+)
-(global-git-gutter+-mode +1)
+(leaf git
+  :ensure magit git-gutter+
+  :config (global-git-gutter+-mode +1))
 
 (defun buffer-list-where (pred)
   "List of all live buffers where PRED holds."
@@ -147,56 +156,62 @@
   (interactive)
   (in-all-buffers-where (-const git-gutter+-mode) (git-gutter+-refresh)))
 
-(package-install 'rg)
+(leaf rg :ensure t)
 
-(package-install 'yasnippet)
-(yas-global-mode)
+(leaf yasnippet
+  :ensure t
+  :config (yas-global-mode +1))
 
-(when unix? (package-install 'fish-mode))
+(leaf fish-mode :when unix? :ensure t)
 
 (set-language-environment "Japanese")
 (prefer-coding-system 'utf-8)
 ;; Ubuntu  -- apt install cmigemo
 ;; Windows -- https://www.kaoriya.net/software/cmigemo/
 ;; M-x customize-variable migemo-dictionary
+(leaf migemo :ensure t)
 (require 'swiper-migemo)
 (add-to-list 'swiper-migemo-enable-command 'counsel-recentf)
 (add-to-list 'swiper-migemo-enable-command 'counsel-rg)
 (setq migemo-options '("--quiet" "--nonewline" "--emacs"))
 (global-swiper-migemo-mode +1)
 
-;; 入力メソッドの設定。
-(require 'agda-input)
-(unless wsl? (setq default-input-method "Agda"))
-;; WSL で Emacs を使っているとき、入力メソッドの切換は次のような状態遷移系にする。
-;; states     : NoIM, Agda, Mozc
-;; inputs     : C-\, <C-henkan>, <C-muhenkan>
-;; transitions:
-;; |      | C-\   | <C-henkan> | <c-muhenkan> |
-;; |------+-------+------------+--------------|
-;; | NoIM | Agda  | Mozc       | NoIM         |
-;; | Agda | NoIME | Mozc       | NoIM         |
-;; | Mozc | Agda  | Mozc       | NoIM         |
-(when wsl?
-  (package-install 'mozc)
-  (setq mozc-candidate-style 'echo-area)
-  (defun im-mozc-on () (interactive) (set-input-method "japanese-mozc"))
-  (defun im-agda-on () (interactive) (set-input-method "Agda"))
-  (defun im-off () (interactive) (set-input-method nil))
-  (defun im-off? () (null current-input-method))
-  (defun im-mozc? () (equal current-input-method "japanese-mozc"))
-  (defun im-agda? () (equal current-input-method "Agda"))
-  (defun im-C-backslash ()
-    (interactive)
-    (cond ((im-agda?) (im-off))
-          (t (im-agda-on))))
-  (defun im-C-henkan () (interactive) (im-mozc-on))
-  (defun im-C-muhenkan () (interactive) (im-off))
-  (global-set-key (kbd "C-\\") #'im-C-backslash)
-  (define-key mozc-mode-map (kbd "C-\\") #'im-C-backslash)
-  (global-set-key (kbd "<C-henkan>") #'im-C-henkan)
-  (global-set-key (kbd "<C-muhenkan>") #'im-C-muhenkan)
-  )
+(leaf input-method
+  :require agda-input
+  :config
+  (unless wsl? (setq default-input-method "Agda"))
+  ;; WSL で Emacs を使っているとき、入力メソッドの切換は次のような状態遷移系にする。
+  ;; states     : NoIM, Agda, Mozc
+  ;; inputs     : C-\, <C-henkan>, <C-muhenkan>
+  ;; transitions:
+  ;; |      | C-\   | <C-henkan> | <c-muhenkan> |
+  ;; |------+-------+------------+--------------|
+  ;; | NoIM | Agda  | Mozc       | NoIM         |
+  ;; | Agda | NoIME | Mozc       | NoIM         |
+  ;; | Mozc | Agda  | Mozc       | NoIM         |
+  (leaf mozc :when wsl?
+    :ensure t
+    :config
+    (setq mozc-candidate-style 'echo-area)
+    (defun im-mozc-on () (interactive) (set-input-method "japanese-mozc"))
+    (defun im-agda-on () (interactive) (set-input-method "Agda"))
+    (defun im-off () (interactive) (set-input-method nil))
+    (defun im-off? () (null current-input-method))
+    (defun im-mozc? () (equal current-input-method "japanese-mozc"))
+    (defun im-agda? () (equal current-input-method "Agda"))
+    (defun im-C-backslash ()
+      (interactive)
+      (cond ((im-agda?) (im-off))
+            (t (im-agda-on))))
+    (defun im-C-henkan () (interactive) (im-mozc-on))
+    (defun im-C-muhenkan () (interactive) (im-off))
+    :bind
+    ((:global-map
+      ("C-\\" . im-C-backslash)
+      ("<C-henkan>" . im-C-henkan)
+      ("<C-muhenkan>" . im-C-muhenkan))
+     (:mozc-mode-map
+      ("C-\\" . im-C-backslash)))))
 
 ;; ターミナルエミュレータの起動
 (defcustom terminal-emulator "gnome-terminal" "Terminal enulator.")
@@ -221,47 +236,61 @@ PATH がディレクトリを指すなら、PATH 自身。
   (cond ((file-regular-p path) (file-name-directory path))
         ((file-directory-p path) path)))
 
-(package-install 'lsp-mode)
-(package-install 'lsp-ui)
+(leaf lsp
+  :ensure lsp-mode lsp-ui
+  :config
+  (leaf lsp-metals
+    :config
+    (when (executable-find "metals-emacs")
+      (setq lsp-metals-server-command "metals-emacs"))))
 
-(package-install 'lsp-metals)
-(when (executable-find "metals-emacs")
-  (setq lsp-metals-server-command "metals-emacs"))
-
-(package-install 'auctex)
-(package-install 'magic-latex-buffer)
-(setq magic-latex-enable-block-align nil
-      magic-latex-enable-inline-image nil)
-(add-hook 'LaTeX-mode-hook (lambda () (when (graphical?) (magic-latex-buffer +1))))
 (defun graphical? () (null (eq (framep (selected-frame)) t)))
-(add-hook 'LaTeX-mode-hook (lambda () (reftex-mode +1)))
-(with-eval-after-load 'tex
-  (require 'reftex)
-  (require 'bibtex)
-  (setq LaTeX-electric-left-right-brace t)
+
+(leaf tex
+  :ensure auctex magic-latex-buffer
+  :custom
+  (magic-latex-enable-block-align . nil)
+  (magic-latex-enable-inline-image . nil)
+  (LaTeX-electric-left-right-brace . t)
+  :hook
+  (LaTeX-mode-hook . (lambda () (when (graphical?) (magic-latex-buffer +1))))
+  (LaTeX-mode-hook . (lambda () (reftex-mode +1)))
+  :config
   (setq TeX-command-list (-remove (lambda (l) (string-equal (car l) "LaTeX")) TeX-command-list))
   (push '("LaTeX" "latexmk --synctex=1 %T" TeX-run-command nil t) TeX-command-list)
-  (setq reftex-default-bibliography `(,(+dropbox-root "lab/bib/ref.bib")) ; TODO
-        bibtex-files '(bibtex-file-path))
-  (setq reftex-label-alist
-        '(("definition" ?d "def:" "~\\ref{%s}" t ("definition" "def.") -3)
-          ("lemma" ?m "lem:" "~\\ref{%s}" t ("lemma" "lem.") -3)
-          ("theorem" ?h "thm:" "~\\ref{%s}" t   ("theorem" "th.") -3)))
+  (leaf reftex
+    :ensure t
+    :custom
+    (reftex-label-alist .
+     '(("definition" ?d "def:" "~\\ref{%s}" t ("definition" "def.") -3)
+       ("lemma" ?m "lem:" "~\\ref{%s}" t ("lemma" "lem.") -3)
+       ("theorem" ?h "thm:" "~\\ref{%s}" t   ("theorem" "th.") -3)))
+    (reftex-default-bibliography . `(,(+dropbox-root "lab/bib/ref.bib"))) ; TODO
+    )
+  (leaf bibtex
+    :ensure t
+    :custom
+    (bibtex-files . '(bibtex-file-path)))
   )
 
-(with-eval-after-load 'org
-  (define-key org-mode-map (kbd "M-p") #'org-move-subtree-up)
-  (define-key org-mode-map (kbd "M-n") #'org-move-subtree-down)
-  (setq org-adapt-indentation nil)
+(leaf org
+  :custom
+  (org-adapt-indentation . nil)
+  (org-agenda-files . `(,(+dropbox-root "org")))
+  (org-agenda-span . 'month)
+  (org-export-use-babel . nil)
+  (org-export-with-broken-link . t)
+  :config
   (defvar org-indent-mode-on-automatically t)
-  (add-hook 'org-mode-hook
-    (lambda () (when org-indent-mode-on-automatically (org-indent-mode +1))))
-  (setq org-agenda-files (list (+dropbox-root "org"))
-        org-agenda-span 'month)
-  (setq org-export-use-babel nil
-        org-export-with-broken-links t))
+  :bind
+  (:org-mode-map
+   ("M-p" . org-move-subtree-up)
+   ("M-n" . org-move-subtree-down))
+  :hook
+  (org-mode-hook . (lambda () (when org-indent-mode-on-automatically (org-indent-mode +1))))
+  )
 
-(package-install 'dockerfile-mode)
+(leaf dockerfile-mode :ensure t)
 
 (defun count-chars-buffer (count?)
   (save-excursion
@@ -349,8 +378,9 @@ PATH がディレクトリを指すなら、PATH 自身。
  ;; [o]rg mode
  "oa" #'org-agenda)
 
-(require 'server)
-(unless (server-running-p) (server-start))
+(leaf server :unless (server-running-p)
+  :config
+  (server-start))
 
 ;; Local Variables:
 ;; indent-tabs-mode: nil
