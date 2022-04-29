897fecd983fa78da996db09322bc48b6965625ec
Masaki Kamano <kamano.m.aa@m.titech.ac.jp>
2022-04-29 17:08:12 +0900

    update

--- install.sh
+++ install.sh
@@ -1,23 +1,13 @@
-#!/bin/bash
+#!/bin/bash -x
 
-# linked from ~
-from_home_dir=(
-    ".Xmodmap"
-)
 target=$HOME/.config
-
-
+mkdir -p $target
 
 git submodule update --init --recursive
-mkdir -p $target
+
 thisdir=$(readlink -f $(dirname $0))
+items=("bat" "emacs" "fish" "git" "latexmk")
 
-for item in $(find $thisdir -maxdepth 1 -mindepth 1); do
-    mv -i ${item} $target
-done
-
-cd $target
-
-for item in "${from_home_dir[@]}"; do
-    ln -siv $(pwd)/${item} $HOME/${item}
+for item in "${items}"; do
+    ln -siv $thisdir/${item} $target/${item}
 done
