5ef82435d5ade269fec1d1effd4465dd0feb360b
Masaki Kamano <kamano.m.aa@m.titech.ac.jp>
2022-05-13 15:56:19 +0900

    [fish] source extra-config.fish if exists

--- fish/config.fish
+++ fish/config.fish
@@ -2,3 +2,5 @@ set -gx XDG_DATA_HOME $HOME/.local/share
 set -gx XDG_CONFIG_HOME $HOME/.config
 set -gx XDG_CACHE_HOME $HOME/.cache
 set -gx MANPATH (string split ':' (manpath 2> /dev/null))
+
+test -e $XDG_CONFIG_HOME/fish/extra-config.fish && . $XDG_CONFIG_HOME/fish/extra-config.fish
