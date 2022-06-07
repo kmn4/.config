e217eade5c2fa9d008c740a3d1df31d8ed677757
Masaki Kamano <kamano.m.aa@m.titech.ac.jp>
2022-06-07 14:52:13 +0900

    fix install.sh

--- install.sh
+++ install.sh
@@ -8,6 +8,6 @@ git submodule update --init --recursive
 thisdir=$(readlink -f $(dirname $0))
 items=("bat" "emacs" "fish" "git" "latexmk")
 
-for item in "${items}"; do
-    ln -siv $thisdir/${item} $target/${item}
+for item in "${items[@]}"; do
+    ln -sivT $thisdir/${item} $target/${item}
 done
