5858b02143713a61421c34b42b4aede48bf00004
Masaki Kamano <kamano.m.aa@m.titech.ac.jp>
2022-11-18 00:25:53 +0900

    [emacs] update .mc-lists.el

--- emacs/.mc-lists.el
+++ emacs/.mc-lists.el
@@ -3,11 +3,30 @@
 
 (setq mc/cmds-to-run-for-all
       '(
-	))
+        TeX-insert-sub-or-superscript
+        forward-sexp
+        backward-sexp
+        backward-up-list
+        delete-horizontal-space
+        down-list
+        kill-sexp
+        mark-sexp
+        markdown-outdent-or-delete
+        org-delete-char
+        org-self-insert-command
+        sp-unwrap-sexp
+        ))
 
 (setq mc/cmds-to-run-once
       '(
-	counsel-M-x
-	mc/mark-next-lines
-	mc/mark-previous-lines
-	))
+        counsel-M-x
+        handle-switch-frame
+        ignore
+        kmacro-end-or-call-macro
+        kmacro-start-macro-or-insert-counter
+        lsp-signature-stop
+        lsp-ui-doc--handle-mouse-movement
+        mc/mark-next-lines
+        mc/mark-previous-lines
+        vterm-or-suspend
+        ))
