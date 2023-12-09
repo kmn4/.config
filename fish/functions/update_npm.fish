d9cdeac21da65134767683e7f4fee51e6c57e1d1
Masaki Kamano <kamano@kamasaki.net>
2023-12-09 19:07:42 +0900

    fix(fish): update_nmp.fish

--- fish/functions/update_npm.fish
+++ fish/functions/update_npm.fish
@@ -1,3 +1,3 @@
 function update_npm --wraps='npm update -g' --description 'alias update_npm npm update -g'
-    which npm > /dev/null && npm update -g $argv; 
+    which npm > /dev/null && sudo npm update -g $argv; 
 end
