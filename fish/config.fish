4961e03691cc279c6944fa97803cbb1b8607eb26
Masaki Kamano <kamano@kamasaki.net>
2023-12-29 15:04:22 +0900

    [fish,git] 1password SSH, Hugo

--- fish/config.fish
+++ fish/config.fish
@@ -6,9 +6,13 @@ set -gx MANPATH (string split ':' (manpath 2> /dev/null))
 set -gx LANG ja_JP.UTF-8
 
 not status is-login || set -gx GPG_TTY (tty)
+status is-login && which hugo > /dev/null && eval (hugo completion fish)
 
 if not set -q WIN_HOME && is-wsl2
     set -U WIN_HOME (wslpath (wslvar USERPROFILE))
 end
+
+alias ssh="ssh.exe"
+alias ssh-add="ssh-add.exe"
 
 test -e $XDG_CONFIG_HOME/fish/extra-config.fish && . $XDG_CONFIG_HOME/fish/extra-config.fish
