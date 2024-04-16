6a32676ddee459e7fe44c2e5008663b3417d68bc
Masaki Kamano <kamano@kamasaki.net>
2024-04-16 23:17:31 +0900

    tidy

--- install.sh
+++ install.sh
@@ -8,8 +8,6 @@ mkdir -p $target
 
 cd $thisdir
 
-git submodule update --init --recursive
-
 for item in "${items[@]}"; do
     [ "$(readlink -f "$target/$item")" = "$thisdir/$item" ] || ln -sivT "$thisdir/$item" "$target/$item"
 done
@@ -22,6 +20,4 @@ for item in "${!files[@]}"; do
     file="${files["$item"]}"
     [ "$(readlink -f "$HOME/$item")" = "$file" ] || ln -sivT "$file" "$HOME/$item"
 done
-
-#which fish >/dev/null && fish -c 'curl -sL https://git.io/fisher | source && fisher install jorgebucaran/fisher && fisher update'
 
