aacb0450bfa50d6b1b4ebf98e0b6ef8f24bb5de5
Masaki Kamano <kamano@kamasaki.net>
2025-03-16 14:44:21 +0900

    transient values

---
+++ emacs/transient/levels.el
@@ -0,0 +1,4 @@
+((magit-commit
+  (magit:--gpg-sign . 1))
+ (magit-rebase
+  (magit:--gpg-sign . 1)))
---
+++ emacs/transient/values.el
@@ -0,0 +1,2 @@
+((magit-commit)
+ (magit-log:magit-log-mode "-n256" "--graph" "--decorate" "--show-signature"))
